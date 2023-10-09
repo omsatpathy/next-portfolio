3f991fac368db606865e26194db483033b198c99
Om Satpathy <omsatpathy115@gmail.com>
2023-10-09 12:45:44 +0530

    Updated Circular Text

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -8,7 +8,7 @@ const HireMe = () => {
     flex items-center justify-center overflow-hidden md:right-8 md:left-auto md:top-0 md:bottom-auto md:absolute
     '>
         <div className='w-48 h-auto flex items-center justify-center relative md:w-24'>
-            <CircularText className={'fill-dark animate-spin-slow dark:fill-light'}/>
+            {/* <CircularText className={'fill-dark animate-spin-slow dark:fill-light'}/> */}
             <Link href='mailto:omsatpathy007@gmail.com'
             className='flex items-center justify-center absolute 
             left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2
